24a882ba04f3f7fd8f01af01c6abb39af4ce7d8d
@lokeshgithu <lokeshgithub786@gmail.com>
2025-06-04 16:13:20 +0530

    Role-based authentication.

--- src/main/java/com/security/SpringShield/controller/GreetingsController.java
+++ src/main/java/com/security/SpringShield/controller/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.security.SpringShield.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String getHello(){
         return "Hello! this is Lukesh Gadhane.";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String getUserRole(){
+        return "Hello User! this is Lukesh Gadhane.";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String getAdminRole(){
+        return "Hello Admin! this is Lukesh Gadhane.";
     }
 }
